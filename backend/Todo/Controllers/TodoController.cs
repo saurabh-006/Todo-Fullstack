be937f88a3d68231bb2e485ad819465bfaf7c935
Saurabh5372 <saurabhshinde2022@gmail.com>
2026-03-15 15:08:32 +0530

    Initial commit - Asp.net + Angular todo

---
+++ backend/Todo/Controllers/TodoController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.model;
+//using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TodoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodos()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> AddTodo(TodoItem todo)
+        {
+            _context.TodoItems.Add(todo);
+            await _context.SaveChangesAsync();
+
+            return todo;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTodo(int id, TodoItem todo)
+        {
+            if (id != todo.Id)
+                return BadRequest();
+
+            _context.Entry(todo).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo(int id)
+        {
+            var todo = await _context.TodoItems.FindAsync(id);
+
+            if (todo == null)
+                return NotFound();
+
+            _context.TodoItems.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ backend/Todo/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.model;
+
+namespace TodoApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
